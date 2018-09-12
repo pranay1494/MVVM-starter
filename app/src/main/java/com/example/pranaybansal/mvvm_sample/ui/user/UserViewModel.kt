29a7a90dc0b96047d72513da415f33ac4e0c14bd
PranayBansal <pranaybansal@PranayBansals-MacBook-Air.local>
2018-09-12 11:19:44 +0530

    improvements

--- app/src/main/java/com/example/pranaybansal/mvvm_sample/ui/user/UserViewModel.kt
+++ app/src/main/java/com/example/pranaybansal/mvvm_sample/ui/user/UserViewModel.kt
@@ -4,14 +4,12 @@ import android.app.Application
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Transformations
-import android.util.Log
-import com.example.pranaybansal.mvvm_sample.data.DataManager
 import com.example.pranaybansal.mvvm_sample.data.remote.model.User
 import com.example.pranaybansal.mvvm_sample.ui.base.BaseViewModel
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
-class UserViewModel @Inject constructor(val dataManager: DataManager,val repository: UserRepository, application: Application, compositeDisposable: CompositeDisposable) : BaseViewModel(application,compositeDisposable) {
+class UserViewModel @Inject constructor(val repository: UserRepository, application: Application, compositeDisposable: CompositeDisposable) : BaseViewModel(application,compositeDisposable) {
 
     private val welcomeMsg = MutableLiveData<String>()
     private var userdata :LiveData<User>
